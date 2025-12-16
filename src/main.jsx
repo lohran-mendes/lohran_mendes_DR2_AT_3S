462fca6a53ee83937cf6d97a192b5a681c7629d3
Lohran Mendes <lohranfm@gmail.com>
2025-12-16 09:02:09 -0300

    feature: add deploy script and update dependencies in package.json

--- src/main.jsx
+++ src/main.jsx
@@ -7,9 +7,11 @@ import { ProductForm } from "./products/form/productForm";
 import { ProductDetail } from "./products/detail/productDetail";
 import { Header } from "./header/header";
 
+const baseUrl = import.meta.env.BASE_URL;
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={baseUrl}>
       <Header />
       <Routes>
         <Route path="/" element={<ProductList />} />
